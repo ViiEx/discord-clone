c928ca9999f204b0766d61f06e2b7f1f1dd247e9
Vasilis Christopoulos <v.christopoulos@divbyzero.gr>
2024-02-20 11:53:14 +0200

    fix: small type

--- components/modals/leave-server-modal.tsx
+++ components/modals/leave-server-modal.tsx
@@ -55,7 +55,7 @@ export const LeaveServerModal = () => {
             ?
           </DialogDescription>
         </DialogHeader>
-        <DialogFooter className="bg-gray-100 ox-6 py-4">
+        <DialogFooter className="bg-gray-100 px-6 py-4">
           <div className="flex items-center justify-between w-full">
             <Button disabled={isLoading} onClick={onClose} variant="ghost">
               Cancel
